07e65468190589afebb4bc327edd54feca4fcf4a
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-10-17 16:14:52 -0400

    buid ignored

---
+++ build/precache-manifest.c63d217debad9a2b3ef16622a3e4f6e0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce4c54879389348004cd8a5b6a6ec0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9066b337651a838e4b9a",
+    "url": "/static/css/2.37c00e0b.chunk.css"
+  },
+  {
+    "revision": "9066b337651a838e4b9a",
+    "url": "/static/js/2.57549dfb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.57549dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19976f591e4ea294912f",
+    "url": "/static/js/main.85325068.chunk.js"
+  },
+  {
+    "revision": "e9b3aec5b931f5177448",
+    "url": "/static/js/runtime-main.98d32e4b.js"
+  },
+  {
+    "revision": "8647135391f71b4fe144274f482c4d3e",
+    "url": "/static/media/493312.86471353.jpg"
+  },
+  {
+    "revision": "fcdd0d70643ce715032dd8ef5d904e47",
+    "url": "/static/media/black-soil-2.fcdd0d70.png"
+  },
+  {
+    "revision": "a09bed9fb40e4be13804963c3a6bf373",
+    "url": "/static/media/harvest_bkg.a09bed9f.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "1ec2243c168530e456d2098f9157fb8f",
+    "url": "/static/media/sunflower_PNG13389.1ec2243c.png"
+  }
+]);
